c38199a9bb66d88c810ad1ac94eb687c7cecbefd
kristiansen616 <52787766+kristiansen616@users.noreply.github.com>
2023-11-22 11:08:42 -0300

    troca de backgroud na home

--- components/Header.vue
+++ components/Header.vue
@@ -33,7 +33,7 @@
       </div>
     </nav>
 
-    <div class="w-full bg-cover bg-center" style="height:41em; background-image: url(https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80);">
+    <div class="w-full bg-cover bg-center" style="height:41em; background-image: url('/imgs/istockphoto-618067194-1024x1024.jpg');">
       <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
         <div class="text-center">
           <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">
